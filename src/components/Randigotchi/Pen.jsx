b0882b2d811b617836713654151c8d0ce427115a
Alex Bunnell and James Hanley <james@>
2018-09-12 16:15:10 -0700

    Finished some things

--- src/components/Randigotchi/Pen.jsx
+++ src/components/Randigotchi/Pen.jsx
@@ -1,24 +1,92 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 import Randi from './Randi';
 
-function Pen(props){
-  return(
-    <div>
-      {props.randiList.map((randi, index) =>
-        <Randi name={randi.name}
-          food={randi.food}
-          play={randi.play}
-          sleep={randi.sleep}
-          key={index} />
-      )}
-    </div>
-  );
-}
+class Pen extends React.Component{
 
-Pen.propTypes = {
-  randiList: PropTypes.array,
-};
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      masterRandiList: [
+        {
+          name: 'Randi',
+          food: 100,
+          play: 100,
+          sleep: 100
+        }
+      ]
+    };
+    this.handleRandiHungry = this.handleRandiHungry.bind(this);
+    this.handleRandiBored = this.handleRandiBored.bind(this);
+    this.handleRandiSleepy = this.handleRandiSleepy.bind(this);
+  }
+
+  randiDiesALittle(){
+    let newMasterRandiList = this.state.masterRandiList.slice();
+    newMasterRandiList.forEach((randi) => {
+      randi.food -= 7,
+      randi.play -= 7,
+      randi.sleep -= 7;
+    });
+    this.setState({masterRandiList: newMasterRandiList});
+  }
+
+  handleRandiHungry(){
+    let newMasterRandiList = this.state.masterRandiList.slice();
+    newMasterRandiList.forEach((randi) =>
+      randi.food += 1
+    );
+    this.setState({masterRandiList: newMasterRandiList});
+  }
+  handleRandiBored(){
+    let newMasterRandiList = this.state.masterRandiList.slice();
+    newMasterRandiList.forEach((randi) =>
+      randi.play += 1
+    );
+    this.setState({masterRandiList: newMasterRandiList});
+  }
+  handleRandiSleepy(){
+    let newMasterRandiList = this.state.masterRandiList.slice();
+    newMasterRandiList.forEach((randi) =>
+      randi.sleep += 1
+    );
+    this.setState({masterRandiList: newMasterRandiList});
+  }
+
+  render()
+  {
+    return(
+      <div>
+        {this.state.masterRandiList.map((randi, index) =>
+          <Randi
+            onRandiHungry={this.handleRandiHungry}
+            onRandiBored={this.handleRandiBored}
+            onRandiSleepy={this.handleRandiSleepy}
+            name={randi.name}
+            food={randi.food}
+            play={randi.play}
+            sleep={randi.sleep}
+            key={index} />
+        )}
+        <p><Link to='/'>Quit</Link></p>
+      </div>
+    );
+  }
+
+  componentDidMount()
+  {
+    this.killRandi = setInterval(() =>
+      this.randiDiesALittle(),
+    5000
+    );
+  }
+
+  componentWillUnmount()
+  {
+    clearInterval(this.killRandi);
+  }
+}
 
 export default Pen;
